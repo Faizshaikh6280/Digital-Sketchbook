c1e24782c7caeb3367422ce1619ab06336ad7c77
Faiz Alam <faizalam2432005@gmail.com>
2024-10-19 15:53:58 +0530

    Board Drawing Functionality Implemented

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,19 +1,68 @@
 import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
 function Board() {
+  const activeMenuItem = useSelector((store) => store.menu.activeMenuItem);
+
   const canvasRef = useRef(null);
+  const shouldDraw = useRef(false);
+  const { color, size } = useSelector((store) => store.toolbox[activeMenuItem]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    // when mounting
+    const changeConfig = () => {
+      ctx.strokeStyle = color;
+      ctx.lineWidth = size;
+    };
+
+    changeConfig();
+  }, [color, size]);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    function handleMousDown(e) {
+      shouldDraw.current = true;
+      ctx.beginPath();
+      ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    function handleMouseMove(e) {
+      if (!shouldDraw.current) return;
+      ctx.lineTo(e.clientX, e.clientY);
+      ctx.stroke();
+    }
+
+    function handleMouseUp(e) {
+      shouldDraw.current = false;
+    }
+
+    canvas.addEventListener("mousedown", handleMousDown);
+
+    canvas.addEventListener("mousemove", handleMouseMove);
+
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    // when mounting
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMousDown);
+
+      canvas.removeEventListener("mousemove", handleMouseMove);
+
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
 
-  return <canvas ref={canvasRef}>Board</canvas>;
+  return <canvas ref={canvasRef}></canvas>;
 }
 
 export default Board;
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,32 +1,47 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.css";
 import { COLORS, MENU_ITEMS } from "@/constant";
+import { changeBrushSize, changeColor } from "@/slice/toolboxSlice";
+import cx from "classnames";
 
 function Toolbox() {
-  function updateBrushSize() {}
+  const dispatch = useDispatch();
   const activeMenuItem = useSelector((store) => store.menu.activeMenuItem);
+
+  const updateBrushSize = function (e) {
+    dispatch(changeBrushSize({ item: activeMenuItem, size: +e.target.value }));
+  };
+
+  const updateColor = function (newColor) {
+    dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+  };
+
+  const { color } = useSelector((store) => store.toolbox[activeMenuItem]);
+
   const isShowStroke = activeMenuItem === MENU_ITEMS.PENCIL;
   const isShowBrush = activeMenuItem === MENU_ITEMS.ERASER || MENU_ITEMS.PENCIL;
-  const isActive = 1;
 
   return (
     <div className={styles.toolboxContainer}>
       {isShowStroke && (
         <div className={styles.toolItem}>
           <h4 className={styles.toolText}>Stroke Color</h4>
-          <div className={styles.itemContainer}>
+          <div
+            className={styles.itemContainer}
+            onClick={(e) => {
+              updateColor(e.target.id);
+            }}
+          >
             {Object.values(COLORS).map((el, indx) => {
               return (
                 <div
-                  className={`${
-                    isActive === indx + 1 ? styles.activeColor : ""
-                  }`} // Additional outline styles
+                  id={el}
+                  className={cx(styles.colorBox, {
+                    [styles.activeColor]: color === el,
+                  })}
                   key={indx}
                   style={{
                     backgroundColor: el,
-                    width: "24px",
-                    height: "24px",
-                    cursor: "pointer",
                   }}
                 />
               );
